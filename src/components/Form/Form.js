068dbaf844e527167ae99bee4d3542b607215b6a
Lukasz Malinowski <lukaszmalinowski@Lukaszs-MacBook-Pro.local>
2020-02-15 22:58:40 -0700

    done fully functional

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -11,16 +11,43 @@ export default class Form extends Comp {
     this.state = {
       name: '',
       price: 0,
-      img: ''
+      img: '',
+      edit: false
     }
     // one function binding for the requirment VVVVVVVVV LOL ;-)
     this.resetFormInputs = this.resetFormInputs.bind(this)
+  }
+
+  componentDidMount() {
+    const { id } = this.props.match.params
+    if (id) this.getItem(id)
+  }
+
+  getItem(id) {
+    axios
+      .get(`${ref.BASE_URL}/${id}`)
+      .then(res => {
+        let product = res.data[0]
+        this.setState({
+          name: product.name,
+          price: product.price,
+          img: product.img,
+          edit: true
+        })
+      })
+      .catch(err => console.log(`get request err: ${err}`))
   }
 
   addItem = obj => {
     axios
       .post(ref.BASE_URL, obj)
       .catch(err => console.log(`post request err: ${err}`))
+  }
+
+  updateItem = (id, obj) => {
+    axios
+      .put(`${ref.BASE_URL}/${id}`, obj)
+      .catch(err => console.log(`put request err: ${err}`))
   }
 
   resetFormInputs() {
@@ -31,26 +58,28 @@ export default class Form extends Comp {
     })
   }
 
+  handleEdit = () => {
+    const { edit } = this.state
+    this.setState({ edit: !edit })
+  }
+
   handleName = val => {
-    this.setState({
-      name: val
-    })
+    this.setState({ name: val })
   }
 
   handlePrice = val => {
-    this.setState({
-      price: val
-    })
+    this.setState({ price: val })
   }
 
   handleImg = val => {
-    this.setState({
-      img: val
-    })
+    this.setState({ img: val })
   }
 
   render() {
-    const { name, price, img } = this.state
+    const { id } = this.props.match.params
+    const { name, price, img, edit } = this.state
+    console.log(edit)
+
     return (
       <section className='Form'>
         <div className='img-container'>
@@ -87,25 +116,36 @@ export default class Form extends Comp {
               onChange={e => this.handlePrice(e.target.value)}
             />
             <div className='button-conatiner-form'>
-              <button
-                className='button-form'
-                onClick={() => this.resetFormInputs()}
-              >
-                Cancel
-              </button>
+              <Link to='/'>
+                <button
+                  className='button-form'
+                  onClick={() => this.resetFormInputs()}
+                >
+                  Cancel
+                </button>
+              </Link>
               {name === '' || img === '' ? (
                 <button
                   className='button-form'
                   onClick={() => alert('Please Fill Out all the Fields!')}
                 >
-                  Add To Inventory
+                  {console.log(edit)}
+                  {edit ? 'Save Changes' : 'Add To Inventory'}
                 </button>
               ) : (
                 <Link to='/'>
                   <button
                     className='button-form'
                     onClick={() => {
-                      this.addItem({ name: name, price: +price, img: img })
+                      if (edit) {
+                        this.updateItem(id, {
+                          name: name,
+                          price: +price,
+                          img: img
+                        })
+                        this.handleEdit()
+                      } else
+                        this.addItem({ name: name, price: +price, img: img })
                       this.resetFormInputs()
                     }}
                   >
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,108 +1,27 @@
-import React, { Component as Comp } from 'react'
+import React from 'react'
+import { Link } from 'react-router-dom'
 import noImg from '../../media/no-image.png'
 import './Product.css'
 
-export default class Product extends Comp {
-  constructor(props) {
-    super(props)
-    this.state = {
-      editing: false,
-      newName: '',
-      newPrice: '',
-      newImg: ''
-    }
-  }
-
-  handleEditChange = () => {
-    const { editing } = this.state
-    this.setState({
-      editing: !editing
-    })
-  }
-
-  handleName = val => {
-    this.setState({
-      newName: val
-    })
-  }
-  handlePrice = val => {
-    this.setState({
-      newPrice: val
-    })
-  }
-  handleImg = val => {
-    this.setState({
-      newImg: val
-    })
-  }
-
-  resetInputs = () => {
-    this.setState({
-      newName: '',
-      newPrice: '',
-      newImg: ''
-    })
-  }
-
-  render() {
-    const { item, deleteFn, updateFn } = this.props
-    const { editing, newName, newImg, newPrice } = this.state
-    return (
-      <div className='Product'>
-        <div className='items-container'>
-          <img src={item.img || noImg} className='img-content' />
-          <div className='text-content'>
-            {editing ? (
-              <div className='editing-container'>
-                <input
-                  onChange={e => this.handleName(e.target.value)}
-                  type='text'
-                />
-                <input
-                  onChange={e => this.handlePrice(e.target.value)}
-                  type='number'
-                />
-                <input
-                  onChange={e => this.handleImg(e.target.value)}
-                  type='text'
-                />
-                <button
-                  className='button'
-                  onClick={() => {
-                    updateFn(item.id, {
-                      name: newName,
-                      price: +newPrice,
-                      img: newImg
-                    })
-                    this.handleEditChange()
-                    this.resetInputs()
-                  }}
-                >
-                  Save
-                </button>
-              </div>
-            ) : (
-              <>
-                <p>{item.name}</p>
-                <p>${item.price}</p>
-                <div className='button-conatiner'>
-                  <button onClick={() => deleteFn(item.id)} className='button'>
-                    Delete
-                  </button>
-                  <button
-                    onClick={() => {
-                      this.handleEditChange()
-                    }}
-                    className='button'
-                  >
-                    Edit
-                  </button>
-                </div>
-              </>
-            )}
+export default function Product(props) {
+  const { item, deleteFn } = props
+  return (
+    <div className='Product'>
+      <div className='items-container'>
+        <img src={item.img || noImg} className='img-content' />
+        <div className='text-content'>
+          <p>{item.name}</p>
+          <p>${item.price}</p>
+          <div className='button-conatiner'>
+            <button onClick={() => deleteFn(item.id)} className='item-button'>
+              Delete
+            </button>
+            <Link to={`/edit/${item.id}`}>
+              <button className='item-button'>Edit</button>
+            </Link>
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
